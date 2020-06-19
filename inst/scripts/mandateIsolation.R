b28ade0cc562b26be6019db05ab6d98473db2e5d
Tim Lucas <timcdlucas@gmail.com>
2020-06-19 13:23:05 +0100

    Add run for q1

--- inst/scripts/mandateIsolation.R
+++ inst/scripts/mandateIsolation.R
@@ -140,78 +140,18 @@ sweep_results <- sweep_results %>%
 
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
-#sweep_results <- readRDS("data-raw/res_20200529_iso.rds")
-
-# 
-# sweep_results %>% 
-#   ggplot(aes(iso_adhere, 1 - pext, colour = control_effectiveness, group = control_effectiveness * contact_adhere)) + 
-#     geom_line() +
-#     facet_wrap( ~ index_R0) 
-# 
-# 
-# sweep_results %>% 
-#   ggplot(aes(self_report, 1 - pext, colour = control_effectiveness, group = control_effectiveness * contact_adhere)) + 
-#   geom_line() +
-#   facet_wrap( ~ index_R0) 
-# 
-# 
-# 
-# sweep_results %>% 
-#   ggplot(aes(self_report, 1 - pext, shape = factor(control_effectiveness), 
-#              group = control_effectiveness * contact_adhere,
-#              colour = contact_adhere)) + 
-#   geom_line() +
-#   geom_point() +
-#   facet_wrap( ~ index_R0) +
-#   ggtitle('Risk of outbreak. Self report and iso adhere are reciprocal.')
-# 
-# 
-# 
-# 
-# sweep_results %>% 
-#   ggplot(aes(iso_adhere, 1 - pext, shape = factor(control_effectiveness), 
-#              group = control_effectiveness * contact_adhere,
-#              colour = contact_adhere)) + 
-#   geom_line() +
-#   geom_point() +
-#   facet_wrap( ~ index_R0) +
-#   ggtitle('Risk of outbreak. Self report and iso adhere are reciprocal.')
-# 
-# 
-# 
-# sweep_results %>% 
-#   filter(contact_adhere == 0.9) %>% 
-#   ggplot(aes(iso_adhere, 1 - pext, colour = factor(control_effectiveness), 
-#              group = control_effectiveness)) + 
-#   geom_line() +
-#   geom_point() +
-#   facet_wrap( ~ index_R0) +
-#   ylab('risk of large outbreak') +
-#   ggtitle('Risk of outbreak. Self report and iso adhere are reciprocal.')
-# 
-# 
-# 
-# sweep_results %>% 
-#   ggplot(aes(self_report, 1 - pext, colour = factor(control_effectiveness), 
-#              group = control_effectiveness)) + 
-#   geom_line() +
-#   geom_point() +
-#   facet_wrap( ~ index_R0) +
-#   ylab('risk of large outbreak') +
-#   ggtitle('Risk of outbreak. Self report and iso adhere are reciprocal.')
-# 
-
 
 sweep_results %>% 
   ggplot(aes(self_report, iso_adhere, fill = 1 - pext,)) + 
   geom_tile() +
-  facet_wrap( ~ index_R0) +
+  facet_grid(control_effectiveness ~ index_R0) +
   scale_fill_viridis_c() +
   labs(fill = 'Risk')
 
 
 r01 <- 
 sweep_results %>% 
+  filter(control_effectiveness == "0.6") %>% 
   filter(index_R0 == 1.1) %>% 
   ggplot(aes(self_report, iso_adhere, fill = 1 - pext,)) + 
   geom_tile() +
@@ -222,6 +162,7 @@ sweep_results %>%
 
 r02 <- 
   sweep_results %>% 
+  filter(control_effectiveness == "0.6") %>% 
   filter(index_R0 == 1.3) %>% 
   ggplot(aes(self_report, iso_adhere, fill = 1 - pext,)) + 
   geom_tile() +
@@ -231,6 +172,45 @@ r02 <-
 
 r03 <- 
   sweep_results %>% 
+  filter(control_effectiveness == "0.6") %>% 
+  filter(index_R0 == 1.5) %>% 
+  ggplot(aes(self_report, iso_adhere, fill = 1 - pext,)) + 
+  geom_tile() +
+  scale_fill_viridis_c() +
+  ggtitle('R0 = 1.5') +
+  labs(fill = 'Risk')
+
+
+(r01 + r02) / (r03 + plot_spacer())
+
+
+
+
+
+r01 <- 
+  sweep_results %>% 
+  filter(control_effectiveness == "0.4") %>% 
+  filter(index_R0 == 1.1) %>% 
+  ggplot(aes(self_report, iso_adhere, fill = 1 - pext,)) + 
+  geom_tile() +
+  scale_fill_viridis_c() +
+  ggtitle('R0 = 1.1') +
+  labs(fill = 'Risk')
+
+
+r02 <- 
+  sweep_results %>% 
+  filter(control_effectiveness == "0.4") %>% 
+  filter(index_R0 == 1.3) %>% 
+  ggplot(aes(self_report, iso_adhere, fill = 1 - pext,)) + 
+  geom_tile() +
+  scale_fill_viridis_c() +
+  ggtitle('R0 = 1.3') +
+  labs(fill = 'Risk')
+
+r03 <- 
+  sweep_results %>% 
+  filter(control_effectiveness == "0.4") %>% 
   filter(index_R0 == 1.5) %>% 
   ggplot(aes(self_report, iso_adhere, fill = 1 - pext,)) + 
   geom_tile() +
@@ -268,4 +248,65 @@ r03 <-
 #   facet_grid(iso_scenario ~ sensitivity) +
 #   ylim(c(0,1))
 
+
+
+no.samples <- 2000
+
+
+
+scenarios2 <- tidyr::expand_grid(
+  ## Put parameters that are grouped by disease into this data.frame
+  delay_group = list(tibble::tibble(
+    delay = c("Adherence"),
+    delay_shape = c(0.9),
+    delay_scale = 1
+  )),
+  inc_meanlog = 1.434065,
+  inc_sdlog = 0.6612,
+  inf_shape = 2.115779,
+  inf_rate = 0.6898583,
+  inf_shift = 3,
+  min_quar_delay = 1,
+  max_quar_delay = c(1),
+  index_R0 = c(1.1,1.3,1.5),
+  prop.asym = c(0.4),
+  control_effectiveness = seq(0.4, 0.8, 0.2),
+  control_effectiveness_base = seq(0.4, 0.8, 0.2),
+  self_report = seq(0.4, 0.9, 0.1),
+  iso_adhere = seq(0.4, 0.9, 0.1),
+  test_delay = c(2), #time from isolation to test result
+  sensitivity = c(0.65), #percent of cases detected
+  precaution = c(0), #this could be between 0 and 7? Number of days stay in isolation if negative test
+  num.initial.cases = c(20)) %>%
+  tidyr::unnest("delay_group") %>%
+  dplyr::mutate(scenario = 1:dplyr::n())
+
+scenarios2$control_effectiveness <- scenarios2$self_report * scenarios2$control_effectiveness
+
+cap_cases <- 2000
+max_days <- 300
+## Parameterise fixed paramters
+sim_with_params <- purrr::partial(scenario_sim,
+                                  cap_max_days = max_days,
+                                  cap_cases = cap_cases,
+                                  r0isolated = 0,
+                                  disp.iso = 1,
+                                  disp.com = 0.16,
+                                  quarantine = TRUE)
+
+future::plan("multicore")
+
+#+ full_run
+tic()
+## Run parameter sweep
+sweep_results2 <- ringbp::parameter_sweep(scenarios2,
+                                          sim_fn = sim_with_params,
+                                          samples = no.samples,
+                                          show_progress = TRUE,
+                                          earlyOut = FALSE)
+toc()
+
+
+
+saveRDS(sweep_results2, file = "data-raw/res_20200617_iso2.rds")
 
