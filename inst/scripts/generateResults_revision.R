5b85fadbf63e41f05485633bab001e5e424397b2
emmalouisedavis <emma.davis@bdi.ox.ac.uk>
2021-01-15 16:47:43 +0000

    Script for revisions Jan 2020

--- inst/scripts/generateResults_revision.R
+++ inst/scripts/generateResults_revision.R
@@ -7,6 +7,7 @@ library(tictoc)
 library(ggplot2)
 library(patchwork)
 library(cowplot)
+library(latex2exp)
 library(furrr)
 library(sn)
 library(ggrepel)
@@ -18,17 +19,17 @@ library(svglite)
 
 rm(list = ls())
 devtools::load_all()
+options(future.rng.onMisuse="ignore")
 
-set.seed(200518)
+set.seed(210115)
 
 no.samples <- 5000
 
-tic()
 scenarios1 <- tidyr::expand_grid(
   ## Put parameters that are grouped by disease into this data.frame
   delay_group = list(tibble::tibble(
     delay = c("Adherence"),
-    delay_shape = c(0.9),
+    delay_shape = c(0.7),
     delay_scale = 1
   )),
   inc_meanlog = 1.434065,
@@ -37,14 +38,15 @@ scenarios1 <- tidyr::expand_grid(
   inf_rate = 1.388388,
   inf_shift = 12.978985,
   min_quar_delay = 1,
-  max_quar_delay = c(1),
-  index_R0 = c(1.1,1.3,1.5),
-  prop.asym = c(0.4),
-  control_effectiveness = c(0,seq(0.4, 1, 0.2)),
+  max_quar_delay = c(1,4),
+  index_R0 = c(1.3,1.5),
+  prop.asym = c(0.31),
+  control_effectiveness = seq(0, 0.8, 0.2),
   self_report = c(0.5),
-  test_delay = c(0,2), #time from isolation to test result
-  sensitivity = c(0.65), #percent of cases detected
-  precaution = c(0), #this could be between 0 and 7? Number of days stay in isolation if negative test
+  iso_adhere = c(0.65),
+  test_delay = c(1,4), #time from isolation to test result
+  sensitivity = c(0.65,0.95), #percent of cases detected
+  precaution = c(0,7), #this could be between 0 and 7? Number of days stay in isolation if negative test
   num.initial.cases = c(5)) %>%
   tidyr::unnest("delay_group") %>%
   dplyr::mutate(scenario = 1:dplyr::n())
@@ -57,8 +59,10 @@ sim_with_params <- purrr::partial(ringbp::scenario_sim,
                                   cap_cases = cap_cases,
                                   r0isolated = 0,
                                   disp.iso = 1,
-                                  disp.com = 0.16,
+                                  disp.com = 0.23,
                                   quarantine = TRUE)
+
+future::plan("multicore")
 
 #+ full_run
 tic()
@@ -66,32 +70,28 @@ tic()
 sweep_results1 <- ringbp::parameter_sweep(scenarios1,
                                           sim_fn = sim_with_params,
                                           samples = no.samples,
-                                          show_progress = TRUE)
+                                          show_progress = TRUE,
+                                          earlyOut=FALSE)
 toc()
 
 
 # #+ writeout
-saveRDS(sweep_results1, file = "data-raw/res_Aug_1.rds")
+saveRDS(sweep_results1, file = "data-raw/res_Jan_1.rds")
 rm(list=ls())
 Sys.sleep(120)
-# tic()
-# sweep_results1 <- readRDS("data-raw/res_Aug_1.rds")
-# toc()
+
 ##################################################################
 
+set.seed(210115)
 
-set.seed(200518)
-
-#' Delay shape is adherence probability
-#'
-#' Cap cases was chosen in a seperate analysis (choose_cap.R or something.)
 no.samples <- 5000
 
+tic()
 scenarios2 <- tidyr::expand_grid(
   ## Put parameters that are grouped by disease into this data.frame
   delay_group = list(tibble::tibble(
     delay = c("Adherence"),
-    delay_shape = c(0.9),
+    delay_shape = c(0.182),
     delay_scale = 1
   )),
   inc_meanlog = 1.434065,
@@ -100,14 +100,75 @@ scenarios2 <- tidyr::expand_grid(
   inf_rate = 1.388388,
   inf_shift = 12.978985,
   min_quar_delay = 1,
-  max_quar_delay = c(1),
-  index_R0 = c(1.1,1.3,1.5),
-  prop.asym = c(0.4),
-  control_effectiveness = c(0,seq(0.4, 1, 0.2)),
+  max_quar_delay = c(1,4),
+  index_R0 = c(1.3,1.5),
+  prop.asym = c(0.31),
+  control_effectiveness = seq(0, 0.8, 0.2),
+  self_report = c(0.119),
+  iso_adhere = c(0.109),
+  test_delay = c(1,4), #time from isolation to test result
+  sensitivity = c(0.65,0.95), #percent of cases detected
+  precaution = c(0,7), #this could be between 0 and 7? Number of days stay in isolation if negative test
+  num.initial.cases = c(5)) %>%
+  tidyr::unnest("delay_group") %>%
+  dplyr::mutate(scenario = 1:dplyr::n())
+
+cap_cases <- 2000
+max_days <- 300
+## Parameterise fixed parameters
+sim_with_params <- purrr::partial(ringbp::scenario_sim,
+                                  cap_max_days = max_days,
+                                  cap_cases = cap_cases,
+                                  r0isolated = 0,
+                                  disp.iso = 1,
+                                  disp.com = 0.23,
+                                  quarantine = TRUE)
+
+future::plan("multicore")
+
+#+ full_run
+tic()
+## Run parameter sweep
+sweep_results2 <- ringbp::parameter_sweep(scenarios2,
+                                          sim_fn = sim_with_params,
+                                          samples = no.samples,
+                                          show_progress = TRUE,
+                                          earlyOut=FALSE)
+toc()
+
+
+# #+ writeout
+saveRDS(sweep_results2, file = "data-raw/res_Jan_2.rds")
+rm(list=ls())
+Sys.sleep(120)
+
+##################################################################
+
+set.seed(210115)
+no.samples <- 5000
+
+scenarios3 <- tidyr::expand_grid(
+  ## Put parameters that are grouped by disease into this data.frame
+  delay_group = list(tibble::tibble(
+    delay = c("Adherence"),
+    delay_shape = c(0.7),
+    delay_scale = 1
+  )),
+  inc_meanlog = 1.434065,
+  inc_sdlog = 0.6612,
+  inf_shape = 17.773185,
+  inf_rate = 1.388388,
+  inf_shift = 12.978985,
+  min_quar_delay = 1,
+  max_quar_delay = c(1,4),
+  index_R0 = c(1.3,1.5),
+  prop.asym = c(0.31),
+  control_effectiveness = seq(0, 0.8, 0.2),
   self_report = c(0.5),
-  test_delay = c(0,2), #time from isolation to test result
-  sensitivity = c(0.65), #percent of cases detected
-  precaution = c(7), #this could be between 0 and 7? Number of days stay in isolation if negative test
+  iso_adhere = c(0.65),
+  test_delay = c(0), #time from isolation to test result
+  sensitivity = c(0), #percent of cases detected
+  precaution = c(0), #this could be between 0 and 7? Number of days stay in isolation if negative test
   num.initial.cases = c(5)) %>%
   tidyr::unnest("delay_group") %>%
   dplyr::mutate(scenario = 1:dplyr::n())
@@ -124,60 +185,6 @@ sim_with_params <- purrr::partial(ringbp::scenario_sim,
                                   quarantine = TRUE)
 
 #+ full_run
-tic()
-## Run parameter sweep
-sweep_results2 <- ringbp::parameter_sweep(scenarios2,
-                                          sim_fn = sim_with_params,
-                                          samples = no.samples,
-                                          show_progress = TRUE)
-toc()
-
-
-# #+ writeout
-saveRDS(sweep_results2, file = "data-raw/res_Aug_2.rds")
-rm(list=ls())
-Sys.sleep(120)
-
-
-set.seed(200518)
-no.samples <- 5000
-
-scenarios3 <- tidyr::expand_grid(
-  ## Put parameters that are grouped by disease into this data.frame
-  delay_group = list(tibble::tibble(
-    delay = c("Adherence"),
-    delay_shape = c(0.9),
-    delay_scale = 1
-  )),
-  inc_meanlog = 1.434065,
-  inc_sdlog = 0.6612,
-  inf_shape = 17.773185,
-  inf_rate = 1.388388,
-  inf_shift = 12.978985,
-  min_quar_delay = 1,
-  max_quar_delay = c(1),
-  index_R0 = c(1.3),
-  prop.asym = c(0.4),
-  control_effectiveness = c(0,seq(0.4, 1, 0.2)),
-  self_report = c(0.5),
-  test_delay = c(0,2), #time from isolation to test result
-  sensitivity = c(0.95), #percent of cases detected
-  precaution = c(0,7), #this could be between 0 and 7? Number of days stay in isolation if negative test
-  num.initial.cases = c(5)) %>%
-  tidyr::unnest("delay_group") %>%
-  dplyr::mutate(scenario = 1:dplyr::n())
-
-cap_cases <- 2000
-max_days <- 300
-## Parameterise fixed paramters
-sim_with_params <- purrr::partial(ringbp::scenario_sim,
-                                  cap_max_days = max_days,
-                                  cap_cases = cap_cases,
-                                  r0isolated = 0,
-                                  disp.iso = 1,
-                                  disp.com = 0.16,
-                                  quarantine = TRUE)
-
 tic()
 ## Run parameter sweep
 sweep_results3 <- ringbp::parameter_sweep(scenarios3,
@@ -187,136 +194,13 @@ sweep_results3 <- ringbp::parameter_sweep(scenarios3,
 
 toc()
 
-# #+ writeout
-saveRDS(sweep_results3, file = "data-raw/res_Aug_3.rds")
-rm(list=ls())
-Sys.sleep(120)
-##################################################################
-
-set.seed(200518)
-no.samples <- 5000
-
-scenarios4 <- tidyr::expand_grid(
-  ## Put parameters that are grouped by disease into this data.frame
-  delay_group = list(tibble::tibble(
-    delay = c("Adherence"),
-    delay_shape = c(0.9),
-    delay_scale = 1
-  )),
-  inc_meanlog = 1.434065,
-  inc_sdlog = 0.6612,
-  inf_shape = 17.773185,
-  inf_rate = 1.388388,
-  inf_shift = 12.978985,
-  min_quar_delay = 1,
-  max_quar_delay = c(4),
-  index_R0 = c(1.3),
-  prop.asym = c(0.4),
-  control_effectiveness = c(0,seq(0.4, 1, 0.2)),
-  self_report = c(0.5),
-  test_delay = c(2), #time from isolation to test result
-  sensitivity = c(0), #percent of cases detected
-  precaution = c(7), #this could be between 0 and 7? Number of days stay in isolation if negative test
-  num.initial.cases = c(5)) %>%
-  tidyr::unnest("delay_group") %>%
-  dplyr::mutate(scenario = 1:dplyr::n())
-
-cap_cases <- 2000
-max_days <- 300
-## Parameterise fixed paramters
-sim_with_params <- purrr::partial(ringbp::scenario_sim,
-                                  cap_max_days = max_days,
-                                  cap_cases = cap_cases,
-                                  r0isolated = 0,
-                                  disp.iso = 1,
-                                  disp.com = 0.16,
-                                  quarantine = TRUE)
-
-#+ full_run
-tic()
-## Run parameter sweep
-sweep_results4 <- ringbp::parameter_sweep(scenarios4,
-                                          sim_fn = sim_with_params,
-                                          samples = no.samples,
-                                          show_progress = TRUE)
-
-toc()
-
 
 # #+ writeout
-saveRDS(sweep_results4, file = "data-raw/res_Aug_4.rds")
+saveRDS(sweep_results3, file = "data-raw/res_Jan_3.rds")
 rm(list=ls())
+
 ##################################################################
 
-#TO RUN:
-rm(list = ls())
-devtools::load_all()
-
-set.seed(200518)
-
-no.samples <- 5000
-
-tic()
-scenarios5 <- tidyr::expand_grid(
-  ## Put parameters that are grouped by disease into this data.frame
-  delay_group = list(tibble::tibble(
-    delay = c("Adherence"),
-    delay_shape = c(0.9),
-    delay_scale = 1
-  )),
-  inc_meanlog = 1.434065,
-  inc_sdlog = 0.6612,
-  inf_shape = 17.773185,
-  inf_rate = 1.388388,
-  inf_shift = 12.978985,
-  min_quar_delay = 1,
-  max_quar_delay = c(4),
-  index_R0 = c(1.1,1.3,1.5),
-  prop.asym = c(0.4),
-  control_effectiveness = c(0,seq(0.4, 1, 0.2)),
-  self_report = c(0.5),
-  test_delay = c(2), #time from isolation to test result
-  sensitivity = c(0.65), #percent of cases detected
-  precaution = c(7), #this could be between 0 and 7? Number of days stay in isolation if negative test
-  num.initial.cases = c(5)) %>%
-  tidyr::unnest("delay_group") %>%
-  dplyr::mutate(scenario = 1:dplyr::n())
-
-cap_cases <- 2000
-max_days <- 300
-## Parameterise fixed parameters
-sim_with_params <- purrr::partial(ringbp::scenario_sim,
-                                  cap_max_days = max_days,
-                                  cap_cases = cap_cases,
-                                  r0isolated = 0,
-                                  disp.iso = 1,
-                                  disp.com = 0.16,
-                                  quarantine = TRUE)
-
-#+ full_run
-tic()
-## Run parameter sweep
-sweep_results5 <- ringbp::parameter_sweep(scenarios5,
-                                          sim_fn = sim_with_params,
-                                          samples = no.samples,
-                                          show_progress = TRUE)
-toc()
-
-
-# #+ writeout
-saveRDS(sweep_results5, file = "data-raw/res_Aug_5.rds")
-rm(list=ls())
-Sys.sleep(120)
-# tic()
-# sweep_results1 <- readRDS("data-raw/res_Aug_1.rds")
-# toc()
-sweep_results <- readRDS("data-raw/res_Aug_complete.rds")
-sweep_results <- rbind(sweep_results,sweep_results5)
-sweep_results$scenario <- 1:nrow(sweep_results)
-saveRDS(sweep_results, file = "data-raw/res_Aug_complete.rds")
-##################################################################
-
-toc()
 
 # Load in separate 4 sets of scenarios and combine
 sweep_results1 <- readRDS("data-raw/res_Aug_1.rds")
